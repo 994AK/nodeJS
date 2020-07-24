d4de57a7f734fb1ff2c5cc115b5d258653479fb2
994AK <1505398065@qq.com>
2020-07-24 10:20:35 +0800

    blog功能已经与controller模块化

--- blog/src/controller/blog.js
+++ blog/src/controller/blog.js
@@ -1,4 +1,11 @@
-//只关心数据
+/*
+* 博客数据
+*  - getList : 获取博客列表
+*  - getDetail : 获取博客内容
+*  - newBlog : 新增一篇博客
+*  - updateBlog : 更新一篇博客
+*  - delBlog : 删除博客
+* */
 const getList = (author, keyword) => {
     //先返回假数据，(格式是正确的)
     return [
@@ -32,7 +39,29 @@ const getDetail = (id) => {
     ]
 }
 
+const newBlog = (blogData = {}) => {
+    //blogData 是一个博客对象 title content属性
+
+    console.log("newBlog... blogData", blogData);
+
+    return {
+        id:3 //表示新建博客，插入到数据表里面的 id
+    }
+}
+
+const updateBlog = (id,blogData={}) =>{
+    return true
+}
+
+const delBlog = (id) =>{
+    // id 就是要删除博客的 id
+    return true
+}
 
 module.exports = {
-    getList
+    getList,
+    getDetail,
+    newBlog,
+    updateBlog,
+    delBlog,
 }
