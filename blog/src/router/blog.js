b3c11bcf20fc83ca2d9e2476ae450c6ad35ed0b8
994AK <1505398065@qq.com>
2020-07-24 10:32:00 +0800

    相关内容

--- blog/src/router/blog.js
+++ blog/src/router/blog.js
@@ -1,17 +1,28 @@
-const  { getList, getDetail } = require('../controller/blog');
-const  { SuccessModel, ErrorModel } = require('../model/resModel')
+const {
+    getList,
+    getDetail,
+    newBlog,
+    updateBlog,
+    delBlog
+} = require('../controller/blog')
 
-const  handleBlogRouter = (req, res) => {
+const {
+    SuccessModel,
+    ErrorModel
+} = require('../model/resModel')
+
+const handleBlogRouter = (req, res) => {
     const method = req.method //GET POST
     const url = req.url
     const path = url.split("?")[0];
+    const id = req.query.id;
 
     //获取博客列表
     if (method === 'GET' && path === '/nodeJS/blog/list') {
         const author = req.query.author || '' // app.js query已经解析过了
         const keyword = req.query.keyword || ''
-        const listData = getList(author,keyword) //返回数据
-        return  new SuccessModel(listData)
+        const listData = getList(author, keyword) //返回数据
+        return new SuccessModel(listData)
     }
 
     //获取博客内容
@@ -23,23 +34,28 @@ const  handleBlogRouter = (req, res) => {
     }
 
     //新增一篇博客
-    if(method === 'POST' && path === '/nodeJS/blog/new') {
-        return {
-            msg:'这是新增一篇博客'
-        }
+    if (method === 'POST' && path === '/nodeJS/blog/new') {
+        const data = newBlog(req.body)
+        return new SuccessModel(data);
     }
 
     //更新一篇博客
-    if(method === 'POST' && path === '/nodeJS/blog/update') {
-        return  {
-            msg : '这是更新一篇博客'
+    if (method === 'POST' && path === '/nodeJS/blog/update') {
+        const result = updateBlog(id, req.body)
+        if(result){
+            return  new SuccessModel()
+        } else {
+            return  new ErrorModel("博客更新失败")
         }
     }
 
     //删除一篇博客
-    if(method === 'POST' && path === '/nodeJS/blog/del') {
-        return  {
-            msg : '删除一篇博客'
+    if (method === 'POST' && path === '/nodeJS/blog/del') {
+        const result = delBlog(id)
+        if(result){
+            return  new SuccessModel()
+        } else {
+            return  new ErrorModel("博客删除失败")
         }
     }
 }
